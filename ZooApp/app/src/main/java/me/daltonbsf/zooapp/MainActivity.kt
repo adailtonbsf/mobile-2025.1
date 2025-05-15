651f5af074606e892a20a33a4d4beec867d48610
Adailton Bezerra <70333667+adailtonbsf@users.noreply.github.com>
2025-05-15 19:42:55 -0300

    add: ZooApp

---
+++ ZooApp/app/src/main/java/me/daltonbsf/zooapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package me.daltonbsf.zooapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import me.daltonbsf.zooapp.navigation.NavGraph
+import me.daltonbsf.zooapp.ui.theme.ZooAppTheme
+
+@ExperimentalMaterial3Api
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavGraph()
+        }
+    }
+}
---
+++ ZooApp/app/src/main/java/me/daltonbsf/zooapp/navigation/NavGraph.kt
@@ -0,0 +1,25 @@
+package me.daltonbsf.zooapp.navigation
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import me.daltonbsf.zooapp.models.animalList
+import me.daltonbsf.zooapp.ui.screens.AnimalScreen
+import me.daltonbsf.zooapp.ui.screens.HomeScreen
+
+@ExperimentalMaterial3Api
+@Composable
+fun NavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") { HomeScreen(onAnimalSelected = { animal -> navController.navigate("animal/${animal.name}") }) }
+        composable("animal/{animal}") { backStackEntry ->
+            val animalName = backStackEntry.arguments?.getString("animal")
+            val selectedAnimal = animalList.first { it.name == animalName }
+            AnimalScreen(selectedAnimal)
+        }
+    }
+}
