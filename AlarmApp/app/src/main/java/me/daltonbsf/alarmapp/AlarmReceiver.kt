01def0387d2c499ebd68c1c8d25d01ac97ad0936
Adailton Bezerra <70333667+adailtonbsf@users.noreply.github.com>
2025-05-27 14:38:56 -0300

    done: alarmapp

---
+++ AlarmApp/app/src/main/java/me/daltonbsf/alarmapp/AlarmReceiver.kt
@@ -0,0 +1,13 @@
+package me.daltonbsf.alarmapp
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Toast.makeText(context, "Alarme Disparado!",
+            Toast.LENGTH_LONG).show()
+    }
+}
