bc04fb9caca5c899d69eac285a247a92d4802daa
Adailton Bezerra <70333667+adailtonbsf@users.noreply.github.com>
2025-06-30 20:10:33 -0300

    add: new UI

--- CRUDItemAPP/app/src/main/java/me/dalton/cruditemapp/MainActivity.kt
+++ CRUDItemAPP/app/src/main/java/me/dalton/cruditemapp/MainActivity.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import me.dalton.cruditemapp.ui.theme.CRUDItemAPPTheme
 import me.dalton.cruditemapp.ui.view.ItemScreen
@@ -28,7 +24,7 @@ class MainActivity : ComponentActivity() {
                         ItemScreen(
                             modifier = Modifier
                                 .padding(paddingValues)
-                                .padding(top = 16.dp)
+                                .padding(horizontal = 16.dp)
                         )
                     }
                 )
--- CRUDItemAPP/app/src/main/java/me/dalton/cruditemapp/ui/view/ItemScreen.kt
+++ CRUDItemAPP/app/src/main/java/me/dalton/cruditemapp/ui/view/ItemScreen.kt
@@ -1,23 +1,31 @@
 package me.dalton.cruditemapp.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -30,82 +38,132 @@ fun ItemScreen(
     modifier: Modifier = Modifier,
     viewModel: ItemViewModel = viewModel()
 ) {
-
     val items by viewModel.items
-
     var title by remember { mutableStateOf(TextFieldValue("")) }
     var description by remember { mutableStateOf(TextFieldValue("")) }
-
-    var showDialog by remember {
-        mutableStateOf(false)
-    }
-
-    var selectedItem by remember {
-        mutableStateOf<Item?>(null)
-    }
+    var showDialog by remember { mutableStateOf(false) }
+    var selectedItem by remember { mutableStateOf<Item?>(null) }
 
     Column(
-        modifier = Modifier.padding(16.dp)
+        modifier = modifier
     ) {
-        TextField(
-            value = title,
-            onValueChange = { title = it },
-            label = { Text(text = "Título") },
-            modifier = Modifier.fillMaxWidth()
+        Text(
+            "CRUD Item App",
+            modifier = Modifier.padding(vertical = 16.dp),
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.primary
         )
 
-        TextField(
-            value = description,
-            onValueChange = { description = it },
-            label = { Text(text = "Descrição") },
-            modifier = Modifier.fillMaxWidth()
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Adicionar Item",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(bottom = 8.dp),
+                fontWeight = FontWeight.Bold
+            )
+            OutlinedTextField(
+                value = title,
+                onValueChange = { title = it },
+                label = { Text(text = "Título") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = description,
+                onValueChange = { description = it },
+                label = { Text(text = "Descrição") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Button(
+                onClick = {
+                    if (title.text.isNotEmpty() && description.text.isNotEmpty()) {
+                        viewModel.addItem(Item(title = title.text, description = description.text))
+                        title = TextFieldValue("")
+                        description = TextFieldValue("")
+                    }
+                },
+                modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+                shape = ShapeDefaults.Small
+            ) {
+                Text(text = "Adicionar")
+            }
 
-        Button(
-            onClick = {
-                if (title.text.isNotEmpty() && description.text.isNotEmpty()) {
-                    viewModel.addItem(
-                        Item(title = title.text, description = description.text)
-                    )
-                    title = TextFieldValue("")
-                    description = TextFieldValue("")
-                }
+            HorizontalDivider(
+                modifier = Modifier.padding(8.dp),
+                thickness = 4.dp
+            )
 
-
-            },
-            modifier = Modifier.padding(8.dp)
-        )
-        {
-            Text(text = "Adicionar")
+            Text(
+                text = "Lista de Itens",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(vertical = 8.dp),
+                fontWeight = FontWeight.Bold
+            )
         }
 
         LazyColumn {
             items(items.size) { index ->
                 val item = items[index]
-
                 Card(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(8.dp)
+                        .padding(bottom = 8.dp)
                 ) {
-                    Column(Modifier.padding(8.dp)) {
-                        Text(text = "Título: ${item.title}")
-                        Text(text = "Descrição: ${item.description}")
-
-                        Row {
-                            Button(onClick = {
-                                viewModel.deleteItem(item.id)
-                            }) {
-                                Text(text = "Delete")
-                            }
-
-                            Spacer(modifier = Modifier.width(8.dp))
-
-                            Button(onClick = {
-                                selectedItem = item
-                                showDialog = true
-                            }) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 4.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Column(
+                            modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.Start
+                        ) {
+                            Text(
+                                text = item.title,
+                                style = MaterialTheme.typography.titleMedium,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Spacer(modifier = Modifier.height(4.dp))
+                            Text(
+                                text = item.description,
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                        }
+                        Column(
+                            horizontalAlignment = Alignment.End,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Button(
+                                onClick = {
+                                    selectedItem = item
+                                    showDialog = true
+                                },
+                                shape = ShapeDefaults.Small,
+                                modifier = Modifier.width(96.dp),
+                            ) {
                                 Text(text = "Update")
+                            }
+                            Button(
+                                onClick = {
+                                    viewModel.deleteItem(item.id)
+                                },
+                                shape = ShapeDefaults.Small,
+                                modifier = Modifier.width(96.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = MaterialTheme.colorScheme.error,
+                                    contentColor = MaterialTheme.colorScheme.onError
+                                )
+                            ) {
+                                Text(text = "Delete")
                             }
                         }
                     }
@@ -132,37 +190,30 @@ fun ItemScreen(
 
 }
 
-
 @Composable
 fun UpdateItemDialog(
     item: Item?,
     onDismiss: () -> Unit,
     onUpdate: (Item) -> Unit
 ) {
-
     if (item == null) return
 
-    var title by remember {
-        mutableStateOf(TextFieldValue(item.title))
-    }
-
-    var description by remember {
-        mutableStateOf(TextFieldValue(item.description))
-    }
+    var title by remember { mutableStateOf(TextFieldValue(item.title)) }
+    var description by remember { mutableStateOf(TextFieldValue(item.description))}
 
     AlertDialog(
         onDismissRequest = onDismiss,
         title = { Text(text = "Editar Item") },
         text = {
             Column {
-                TextField(
+                OutlinedTextField(
                     value = title,
                     onValueChange = { title = it },
                     label = { Text(text = "Título") },
                     modifier = Modifier.fillMaxWidth()
                 )
-
-                TextField(
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedTextField(
                     value = description,
                     onValueChange = { description = it },
                     label = { Text(text = "Descrição") },
@@ -171,15 +222,25 @@ fun UpdateItemDialog(
             }
         },
         confirmButton = {
-            Button(onClick = {
-                onUpdate(item.copy(title = title.text, description = description.text))
-            }) {
+            Button(
+                onClick = {
+                    onUpdate(item.copy(title = title.text, description = description.text))
+                },
+                shape = ShapeDefaults.Small
+            ) {
                 Text(text = "Salvar")
             }
 
         },
         dismissButton = {
-            Button(onClick = onDismiss) {
+            Button(
+                onClick = onDismiss,
+                shape = ShapeDefaults.Small,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.secondary,
+                    contentColor = MaterialTheme.colorScheme.onSecondary
+                )
+            ) {
                 Text(text = "Cancelar")
             }
         }
